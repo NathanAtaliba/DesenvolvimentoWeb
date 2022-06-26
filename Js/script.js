da9f878488ce4c724d0e333ddf5050fbc1e98a18
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2022-06-26 18:00:57 -0300

    Projeto

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,5 @@
-document.querySelector(".container").addEventListener("click",()=>
+var hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click",function(){
  document.querySelector(".container").classList.toggle("show-menu")
-);
+});
